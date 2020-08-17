a806c9457a42efbf1ca7b8188721d336c1758073
mirabellej <mirabellejones@gmail.com>
2020-08-17 20:05:25 +0200

    Add files via upload

--- 2_CapTouch_Instrument.ino
+++ 2_CapTouch_Instrument.ino
@@ -17,36 +17,43 @@ void takeAction(uint8_t pad) {
     //  Labeled Pin A4
     case 3:
       CircuitPlayground.playTone(5000, 500);
+      CircuitPlayground.setPixelColor(0, 255,   0,   0);
       break;
       
     //  Labeled Pin A5
     case 2:
       CircuitPlayground.playTone(4000, 500);
+      CircuitPlayground.setPixelColor(1, 255,   0,   0);
       break;
       
     //  Labeled Pin A6
     case 0:
       CircuitPlayground.playTone(1000, 500);
+      CircuitPlayground.setPixelColor(3, 255,   0,   0);
       break;
       
     //  Labeled Pin A7
     case 1:
       CircuitPlayground.playTone(800, 500);
+      CircuitPlayground.setPixelColor(4, 255,   0,   0);
       break;
       
     // Labeled Pin A1
     case 6:
       CircuitPlayground.playTone(300, 500);
+      CircuitPlayground.setPixelColor(6, 255,   0,   0);
       break;
       
     // Labeled Pin A2 
     case 9:
       CircuitPlayground.playTone(200, 500);
+      CircuitPlayground.setPixelColor(8, 255,   0,   0);
       break;
       
     // Labeled Pin A3 
     case 10:
       CircuitPlayground.playTone(100, 500);
+      CircuitPlayground.setPixelColor(9, 255,   0,   0);
       break;
       
     // It's nice to include a default case... just in case! 
@@ -84,6 +91,7 @@ void loop() {
       
       // But not too often.
       delay(DEBOUNCE);
+      CircuitPlayground.clearPixels();
     }
   }
 }
